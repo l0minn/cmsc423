71f0ef5cc5d86005a21058d5aa66238447fe6f72
Neo Zhao <l0minn@terpmail.umd.edu>
2021-02-03 18:15:44 -0500

    panic and print input

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,10 @@ use std::fs;
 fn main() {
     
     //read input file contents into a string called contents
-    let contents = fs::read_to_string("input").expect("Unable to read file");
+    let input = fs::read_to_string("input").expect("Unable to read file");
     let output_buffer = String::new();
     
     //Processing code here (use push or push_str to add to buffer contents)
 
-    /* sample code */
-
-    //compile buffer into a str and write in output file
-    let output = output_buffer.as_str()
-    fs::write("output", output).expect("Unable to write file");
+    panic!("{}", input);
 }
