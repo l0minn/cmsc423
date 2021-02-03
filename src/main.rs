ecb45d2c0cf3261efbe67ef643e8492f2556569a
Neo Zhao <l0minn@terpmail.umd.edu>
2021-02-03 11:17:31 -0500

    refined input and output

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,13 @@ fn main() {
     
     //read input file contents into a string called contents
     let contents = fs::read_to_string("input").expect("Unable to read file");
+    let output_buffer = String::new();
+    
+    //Processing code here (use push or push_str to add to buffer contents)
 
-    //count letters (ignores if not one of the 4 match options)
-    let mut a_count = 0;
-    let mut c_count = 0;
-    let mut g_count = 0;
-    let mut t_count = 0;
-    for c in contents.chars() {
-        match c {
-            'A' => a_count = a_count + 1,
-            'C' => c_count = c_count + 1,
-            'G' => g_count = g_count + 1,
-            'T' => t_count = t_count + 1,
-            _ => (),
-        }
-    }
+    /* sample code */
 
-    //write count results to output file
-    let output_data = format!("{} {} {} {}", a_count, c_count, g_count, t_count);
-    fs::write("output", output_data).expect("Unable to write file");
+    //compile buffer into a str and write in output file
+    let output = output_buffer.as_str()
+    fs::write("output", output).expect("Unable to write file");
 }
