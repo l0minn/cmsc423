2badf8ad0e22b599711f718e5cb57cc0846d8205
Neo Zhao <l0minn@terpmail.umd.edu>
2021-02-03 18:42:51 -0500

    byte panic

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::fs;
 fn main() {
     
     //read input file contents into a string called contents
-    let input = fs::read_to_string("input").expect("Unable to read file");
+    let input = fs::read("input").expect("Unable to read file");
     
     //Processing code here (use push or push_str to add to buffer contents)
 
-    panic!("{}\n", input);
+    panic!("{:X?}", input);
 }
