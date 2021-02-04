5c0dd0b295720ad8f0e2f0bb6abb308f635693f2
Neo Zhao <l0minn@terpmail.umd.edu>
2021-02-03 19:38:14 -0500

    changed var name from content to input

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     
     //read input file contents into a string called contents
-    let contents = fs::read_to_string("input").expect("Unable to read file");
+    let input = fs::read_to_string("input").expect("Unable to read file");
     let output_buffer = String::new();
     
     //Processing code here (use push or push_str to add to buffer contents)
