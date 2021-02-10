73186f3d9c1d868d4bdc9ddc1fee2490f09774e0
Neo Zhao <l0minn@terpmail.umd.edu>
2021-02-10 00:59:53 -0500

    initial complete

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,42 @@ use std::fs;
 fn main() {
     
     //read input file contents into a string called contents
-    let input = fs::read_to_string("input").expect("Unable to read file");
-    let output_buffer = String::new();
+    let mut input = fs::read_to_string("input").expect("Unable to read file");
+    let mut output_buffer = String::new();
     
     //Processing code here (use push or push_str to add to buffer contents)
+    input.push('G');
+    let mut min_locs = Vec::new();
+    let mut min_skew = 0;
+    let mut prev_skew;
+    let mut running_skew = 0;
+    let mut index = 0;
 
-    /* sample code */
+    for c in input.chars() {
+        prev_skew = running_skew;
+        if c == 'C' {
+            running_skew -= 1;
+        } else if c == 'G' {
+            running_skew += 1;
+        }
+
+        if prev_skew < running_skew{
+            if prev_skew < min_skew {
+                min_locs.clear();
+                min_locs.push(index);
+                min_skew = prev_skew;
+            } else if prev_skew == min_skew {
+                min_locs.push(index);
+            }
+        }
+        index += 1;
+    }
+
+    for i in min_locs{
+        output_buffer.push_str(format!("{} ", i).as_str());
+    }
 
     //compile buffer into a str and write in output file
-    let output = output_buffer.as_str()
+    let output = output_buffer.trim_end();
     fs::write("output", output).expect("Unable to write file");
 }
