afc4ea13c1f6735972e050e8e54f49a17e9e4e37
Neo Zhao <l0minn@terpmail.umd.edu>
2021-02-11 01:05:46 -0500

    handled edge cases

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,51 @@ use std::fs;
 fn main() {
     
     //read input file contents into a string called contents
-    let mut input = fs::read_to_string("input").expect("Unable to read file");
+    let input = fs::read_to_string("input").expect("Unable to read file");
     let mut output_buffer = String::new();
     
     //Processing code here (use push or push_str to add to buffer contents)
-    input.push('G');
-    let mut min_locs = Vec::new();
-    let mut min_skew = 0;
-    let mut prev_skew;
+    let mut skew_vals = Vec::new();
     let mut running_skew = 0;
-    let mut index = 0;
-
     for c in input.chars() {
-
-        prev_skew = running_skew;
-        if c == 'C' {
-            running_skew -= 1;
-        } else if c == 'G' {
-            running_skew += 1;
+        if c == 'C' || c == 'G' || c == 'A' || c == 'T' {
+            if c == 'C' {
+                running_skew -= 1;
+            } else if c == 'G' {
+                running_skew += 1;
+            }
+            skew_vals.push(running_skew);
         }
+    }
 
-        if prev_skew < running_skew{
-            if prev_skew < min_skew {
-                min_locs.clear();
-                if index != 0 {
-                    min_locs.push(index);
-                }             
-                min_skew = prev_skew;
-            } else if prev_skew == min_skew && index != 0 {
-                min_locs.push(index);
+    let mut min_skew = skew_vals[0];
+    let mut min_locs = Vec::new();
+
+    for i in 0..skew_vals.len() {
+        if skew_vals[i] < min_skew {
+            min_locs.clear();
+            min_locs.push(i + 1);
+            min_skew = skew_vals[i];
+        } else if skew_vals[i] == min_skew {
+            if min_locs.is_empty() {
+                min_locs.push(i + 1);
+            } else {
+                let temp = min_locs.pop().unwrap();
+                if temp == i - 1 {
+                    min_locs.push(i + 1);
+                } else {
+                    min_locs.push(temp);
+                    min_locs.push(i + 1);
+                }
             }
         }
-        index += 1;
     }
 
-    if min_locs.is_empty() {
-        min_locs.push(1);
-    }
-
-    for i in min_locs{
+    for i in min_locs {
+        print!("{} ", i);
         output_buffer.push_str(format!("{} ", i).as_str());
     }
+    println!();
 
     //compile buffer into a str and write in output file
     let output = output_buffer.trim_end();
