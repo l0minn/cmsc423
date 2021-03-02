173d59accd18ff312b727a2ad534385d1a9e06d9
Neo Zhao <l0minn@terpmail.umd.edu>
2021-03-01 23:19:21 -0500

    made output_buffer mut

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     
     //read input file contents into a string called contents
     let input = fs::read_to_string("input").expect("Unable to read file");
-    let output_buffer = String::new();
+    let mut output_buffer = String::new();
     
     //Processing code here (use push or push_str to add to buffer contents)
 
