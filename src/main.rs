0b2fe7e342e2a1267d1fcfddcda8621d7d3b9eba
l0minn <71852043+l0minn@users.noreply.github.com>
2021-04-13 21:04:20 -0400

    handle multiline input

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     
     //read input file contents into a string called contents
     let input = fs::read_to_string("input").expect("Unable to read file");
+    let processed_inpput:String = input.split_ascii_whitespace().collect();
     let mut output_buffer = String::new();
     
     //Processing code here (use push or push_str to add to buffer contents)
